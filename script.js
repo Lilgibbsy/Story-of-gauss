147e3498161dca828e8d615b997acd2379641c24
Lilgibbsy <owen@gibbsbunch.com>
2022-12-13 14:00:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Script.js
+
+//HTML Elements
+let calcBtn = document.getElementById("clac-btn");
+
+// Add Event Listener
+calcBtn.addEventListener("click", calcSum);
+
+//Event Function
+function calcSum() {
+  let total = 0;
+  for (let n = 1; n <= 100; n++) {
+    total = total + n;
+  }
+
+  //Output the Total
+  sumOut.innerHTML = total;
+}
